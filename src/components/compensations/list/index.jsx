1cb3efe54556ca98b4d7d902d79c3797efc94a8a
Дмитрий Проскурин <proskurin-d@MacBook-Pro-Dmitrij.local>
2019-11-11 22:33:49 +0300

    fade animation comps list

--- src/components/compensations/list/index.jsx
+++ src/components/compensations/list/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles.css'
 import {ItemList} from "../itemList";
 import PropTypes from 'prop-types';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export class List extends React.Component {
     constructor(props) {
@@ -11,7 +12,13 @@ export class List extends React.Component {
     render() {
         return (
             <div className={"List"}>
-                {this.props.items.map((i) => i)}
+                <ReactCSSTransitionGroup
+                    transitionName="fade"
+                    transitionAppear={true}
+                    transitionAppearTimeout={400}
+                >
+                    {this.props.items.map((i) => i)}
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
