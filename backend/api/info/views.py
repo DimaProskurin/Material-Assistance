31ca11e6cfd25d7b75be5871646b42a965e96e87
Дмитрий Проскурин <proskurin-d@MacBook-Pro-Dmitrij.local>
2019-12-07 01:37:43 +0300

    regexp

--- backend/api/info/views.py
+++ backend/api/info/views.py
@@ -3,12 +3,16 @@ from rest_framework.views import APIView
 from .models import Category, Compensation
 from .serializers import CategorySerializer, CompensationSerializer
 from rest_framework.authtoken.models import Token
+import re
 
 
 class CategoriesView(APIView):
     def get(self, request):
-        token = request.COOKIES.get('money_api_token')
+        tokenTuple = request.COOKIES.get('money_api_token')
+        matches = re.search(r'(<Token: (\S*)>)', tokenTuple)
+        token = matches.groups(0)[1]
         user = Token.objects.filter(key=token).last().user
+
         params = dict(request.query_params)
         if 'url' in params:
             response = Category.objects.filter(url__in=params['url'])
